5e37aaddce10d775edd411dd794a7d868de3a7db
ramiiar <33881745+ramiiar@users.noreply.github.com>
2017-11-21 15:55:15 -0800

    Bot

---
+++ config.lua
@@ -0,0 +1,37 @@
+do local _ = {
+  enabled_plugins = {
+    "addrem",
+    "bc",
+    "supergroup",
+    "msg_checks",
+    "pin",
+    "owner",
+    "online",
+    "plugins",
+    "admin",
+    "id",
+    "del",
+    "clean",
+    "expiretime",
+    "filter",
+    "setlink",
+    "setrules",
+    "help",
+    "leave",
+    "lock_bots",
+    "me"
+  },
+  group = {
+    data = "bot/group.json"
+  },
+  robot = {
+    397780135,
+    0
+  },
+  sudo_users = {
+    325882828,
+    0
+  }
+}
+return _
+end
